5e74bc459a2fa980391f7b8042906c74f1fed125
ErikZ58 <96550756+ErikZ58@users.noreply.github.com>
2021-12-22 22:52:08 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 export DISPLAY=10.49.34.211:0.0
-ssh -X -o "StrictHostKeyChecking no" -o ConnectTimeout=2 shiftl@xtd168.erfurt.elex.be 'bash -s' < /cygdrive/c/Users/eze/test2.sh
+ssh -X -o "StrictHostKeyChecking no" -o ConnectTimeout=2 shiftl@abc123.vod.de 'bash -s' < /cygdrive/c/Users/eze/test2.sh
